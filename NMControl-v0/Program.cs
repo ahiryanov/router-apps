c0ffcf27cb65c7f49fe7a55d5e5442ec02ddf71f
Anton Khirianov <ahiryanov@gmail.com>
2022-11-29 15:33:29 +0300

    Add new version of NMControl

---
+++ NMControl-v0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NMControl_v0;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("nmcli -f DEVICE,STATE -t device".Bash());
+    }
+}
+public static class ShellHelper
+{
+    public static string Bash(this string cmd)
+    {
+        var escapedArgs = cmd.Replace("\"", "\\\"");
+
+        var process = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c \"{escapedArgs}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+        string result = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        return result;
+    }
+}
