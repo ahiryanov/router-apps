593f7bc046346a86d1b8af72862139d220b1528d
Anton Khirianov <ahiryanov@gmail.com>
2022-11-30 14:04:37 +0300

    Add checking connections and restart them if needed

--- NMControl-v0/Program.cs
+++ NMControl-v0/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
-using System.Globalization;
-using System.Net.Http.Headers;
-using System.Reflection.Metadata.Ecma335;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Timers;
 
 namespace NMControl_v0;
 class Program
@@ -20,13 +23,58 @@ class Program
             var device = new Device();
             device.Name = device_raw.Split(":")[0];
             device.State = device_raw.Split(":")[1];
-            if(device.Name.Contains("cdc-wdm"))
-                device.Iface = $"qmicli --silent -d /dev/{device.Name} --get-wwan-iface".Bash().Replace("\n","");
-            devices.Add(device);
+            if (device.Name.Contains("cdc-wdm"))
+            {
+                device.Iface = $"qmicli --silent -d /dev/{device.Name} --get-wwan-iface".Bash().Replace("\n", "");
+                devices.Add(device);
+            }
         }
-        
+        devices = devices.OrderBy(m=>m.Name).ToList();
         foreach (var device in devices)
-            Console.WriteLine(device.ToString());
+        {
+            switch(device.State)
+            {
+                case "connected":
+                    Console.WriteLine($"Stay tune with {device.Name}. State {device.State}");
+                    break;
+                case "connecting (prepare)":
+                    Console.WriteLine($"Trying reset connection {device.Name}. Reason: {device.State}");
+                    ConnectionDown(device.Name);
+                    Thread.Sleep(2000);
+                    var prepareResponse = ConnectionUp(device.Name);
+                    if(prepareResponse.ToLower().Contains("failed") || prepareResponse.ToLower().Contains("timeout"))
+                    {
+                        Console.WriteLine($"Failed activation of connecting (prepare) {device.Name}");
+                        ConnectionDown(device.Name);
+                    }
+                    else
+                        Console.WriteLine($"Success activation of connecting (prepare) {device.Name}");
+                    break;
+                case "disconnected":
+                    Console.WriteLine($"Trying reset connection {device.Name}. Reason: {device.State}");
+                    var disconnectResponse = ConnectionUp(device.Name);
+                    if (disconnectResponse.ToLower().Contains("failed") || disconnectResponse.ToLower().Contains("timeout"))
+                    {
+                        Console.WriteLine($"Activation failed of disconnected {device.Name}");
+                        ConnectionDown(device.Name);
+                    }
+                    else
+                        Console.WriteLine($"Success activation of disconnected {device.Name}");
+                    break;
+                case "unavailable":
+                    Console.WriteLine($"Stay tune with {device.Name}. State {device.State}");
+                    break;
+            }
+        }
+    }
+
+    static string ConnectionDown(string deviceName)
+    {
+        return $"nmcli -w 30 connection down {deviceName}-conn 2>&1".Bash();
+    }
+    static string ConnectionUp(string deviceName)
+    {
+        return $"nmcli -w 30 connection up {deviceName}-conn 2>&1".Bash();
     }
 }
 class Device
