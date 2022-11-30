c41004341d8a046f30567dab73b4a7f2db093e5b
Anton Khirianov <ahiryanov@gmail.com>
2022-11-30 11:05:18 +0300

    Add logic

--- NMControl-v0/Program.cs
+++ NMControl-v0/Program.cs
@@ -1,22 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
 
 namespace NMControl_v0;
 class Program
 {
     static void Main(string[] args)
     {
+        var devices = new List<Device>();
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("nmcli -f DEVICE,STATE -t device".Bash());
+        var devices_raw = "nmcli -f DEVICE,STATE -t device".Bash().Split('\r', '\n');
+        foreach (var device_raw in devices_raw)
+        {
+            if (string.IsNullOrWhiteSpace(device_raw))
+                continue;
+            var device = new Device();
+            device.Name = device_raw.Split(":")[0];
+            device.State = device_raw.Split(":")[1];
+            if(device.Name.Contains("cdc-wdm"))
+                device.Iface = $"qmicli --silent -d /dev/{device.Name} --get-wwan-iface".Bash().Replace("\n","");
+            devices.Add(device);
+        }
+        
+        foreach (var device in devices)
+            Console.WriteLine(device.ToString());
+    }
+}
+class Device
+{
+    public string Name { get; set; }
+    public string State { get; set; }
+    public string Iface { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} {State} {Iface}";
     }
 }
 public static class ShellHelper
 {
     public static string Bash(this string cmd)
     {
-        var escapedArgs = cmd.Replace("\"", "\\\"");
+        string escapedArgs = cmd.Replace("\"", "\\\"");
 
-        var process = new Process()
+        Process process = new Process()
         {
             StartInfo = new ProcessStartInfo
             {
