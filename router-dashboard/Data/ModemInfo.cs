1ce9a5589737de10170edd4d6b7c97b2168a7258
ahiryanov <ahiryanov@gmail.com>
2023-03-02 11:18:16 +0300

    Add minimal functions to work in QMI machines

--- router-dashboard/Data/ModemInfo.cs
+++ router-dashboard/Data/ModemInfo.cs
@@ -1,15 +1,17 @@
 ﻿using System.Net;
+using System.Text.Json.Nodes;
 
 namespace router_dashboard.Data
 {
     public class ModemInfo
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public IPAddress? Ip { get; set; }
         public string? Opsos { get; set; }
         public string? WorkMode { get; set; }
         public string? Rssi { get; set; }
         public string? Sinr { get; set; }
         public string? Iccid { get; set; }
+        public string? Status { get; set; }
     }
 }
--- router-dashboard/Data/SystemdService.cs
+++ router-dashboard/Data/SystemdService.cs
@@ -1,32 +1,54 @@
-﻿using System.Diagnostics;
-
-namespace router_dashboard.Data
+﻿namespace router_dashboard.Data;
+public class SystemdService
 {
-    public class SystemdService
+    private readonly Dictionary<string, string> _CheckProcesses = new()
     {
-        Dictionary<string,string> _checkProcesses = new()
+        { "dnsmasq.service", "DNS & DHCP Dnsmasq" },
+        { "chilli.service", "CoovaChilli" },
+        { "nginx.service", "Nginx" },
+        { "openvpn@client.service", "OpenVPN" },
+        { "gps.service", "GPS" },
+        { "zabbix-proxy.service", "Zabbix Proxy" },
+        { "zabbix-agent.service", "Zabbix Agent" },
+        { "lte-reboot.timer", "LTE reboot" },
+        { "dashboard.service", "Dashboard" },
+    };
+    public SystemdService()
+    {
+        var machine = File.ReadLines("/etc/os-release")
+            .FirstOrDefault(t => t.Contains("MACHINE"))?
+            .Split("=")[1]
+            .Replace("\"", "");
+        switch (machine)
         {
-            { "dnsmasq.service", "DNS & DHCP"},
-            { "chilli.service", "CoovaChilli"},
-            { "openvpn@client.service", "OpenVPN"},
-            { "gps.service","GPS"},
-            { "zabbix-proxy.service","Zabbix Proxy"},
-            { "zabbix-agent.service","Zabbix Agent"},
-            { "lte-reboot.timer","LTE reboot"},
-            { "lte-check-gateways.timer","LTE check gateways"}
-        };
-
-        public List<(string Process, bool IsActive)> GetStatuses()
-        {
-            List<(string, bool)> statuses = new();
-            foreach (var process in _checkProcesses)
-            {
-                var response = $"systemctl is-active {process.Key}".Bash();
-                statuses.Add((process.Value,response == "active"));
-            }
-            return statuses;
+            case "yocto-arm64-imx":
+            case "yocto-arm64-nanopi":
+                _CheckProcesses.Add("lte-check-flow-count.timer", "Check flow count");
+                break;
+            case "yocto-x86-e3372":
+            case "yocto-x86-qmi":
+                _CheckProcesses.Add("lte-check-gateways.timer", "LTE check gateways");
+                break;
         }
-
     }
-
+    public List<(string Process, bool IsActive)> GetStatuses()
+    {
+        List<(string, bool)> statuses = new();
+        foreach (var process in _CheckProcesses)
+        {
+            bool isActive;
+            var response = $"systemctl is-active {process.Key}".Bash();
+            if (process.Key.EndsWith("timer"))
+            {
+                var responseService = $"systemctl is-active {process.Key.Replace("timer", "service")}".Bash();
+                isActive = responseService == "inactive" && response == "active";
+            }
+            else
+            {
+                isActive = response == "active";
+            }
+            statuses.Add((process.Value, isActive));
+        }
+        return statuses;
+    }
 }
