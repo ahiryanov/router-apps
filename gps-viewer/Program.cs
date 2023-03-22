143a9af49b4b77d2eb63c5b615a51bd02a15600a
ahiryanov <42962076+ahiryanov@users.noreply.github.com>
2023-03-22 16:28:18 +0300

    Add new device ttyGPS0 for yocto-x86-qmi image

--- gps-viewer/Program.cs
+++ gps-viewer/Program.cs
@@ -44,7 +44,7 @@ public class Program
 
 public class GetGps : BackgroundService
 {
-    private static readonly string[] Serials = { "/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttymxc1", "/dev/ttyS1" };
+    private static readonly string[] Serials = { "/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttymxc1", "/dev/ttyS1", "/dev/ttyGPS0" };
     private string _CurrentPort;
     public static GpsPosition Current;
     private static SerialPort _port;
