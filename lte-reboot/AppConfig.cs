1efbea177acf7056edafd2149f5d5309af49efcd
ahiryanov <ahiryanov@gmail.com>
2026-04-23 12:26:51 +0300

    Incrase restart timeout

--- lte-reboot/AppConfig.cs
+++ lte-reboot/AppConfig.cs
@@ -7,7 +7,7 @@ internal static class AppConfig
 	internal static string Srv;
 	internal static int MaxRtt = 300;
 	internal static int MaxLoss = 25;
-	internal const int RestartCount = 20;
+	internal const int RestartCount = 30;
 	internal const int CooldownCycles = 5;
 	internal const string LogFile = "/tmp/lte";
 
