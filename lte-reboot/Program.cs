c26e82ceddbff8699efcb5fcc8c1004c52107489
ahiryanov <ahiryanov@gmail.com>
2026-05-28 15:39:56 +0300

    Add missing using

--- lte-reboot/Program.cs
+++ lte-reboot/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
